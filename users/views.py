dc444be129d9226240acb42969a28fec24185750
Nitesh Rawat <niteshrawat99@gmail.com>
2018-11-09 18:33:09 +0530

    completed blog functionality

--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
+
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from django.contrib.messages.views import SuccessMessageMixin
+
+
 
 
 def register(request):
